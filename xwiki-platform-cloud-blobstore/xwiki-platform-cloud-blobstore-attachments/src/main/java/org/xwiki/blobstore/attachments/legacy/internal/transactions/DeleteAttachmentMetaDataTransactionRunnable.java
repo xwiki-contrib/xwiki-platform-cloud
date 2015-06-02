f7a10262bff4da3a8d52056fd49c2373d2334b49
Jonathan A. Kennedy <jonathan_kennedy@hms.harvard.edu>
2015-06-02 15:40:39 -0400

    Fix for error in deleting attachment metadata.

--- xwiki-platform-cloud-blobstore/xwiki-platform-cloud-blobstore-attachments/src/main/java/org/xwiki/blobstore/attachments/legacy/internal/transactions/DeleteAttachmentMetaDataTransactionRunnable.java
+++ xwiki-platform-cloud-blobstore/xwiki-platform-cloud-blobstore-attachments/src/main/java/org/xwiki/blobstore/attachments/legacy/internal/transactions/DeleteAttachmentMetaDataTransactionRunnable.java
@@ -53,15 +53,21 @@ public class DeleteAttachmentMetaDataTransactionRunnable extends TransactionRunn
     private XWikiContext xwikiContext;
 
     /**
+     * Indicates whether the doc object should be saved - usually not if called from another transaction.
+     */
+    private boolean updateDoc;
+
+    /**
      * Constructor.
      * 
      * @param xwikiAttachment The XWiki attachment.
      * @param xwikiContext The XWiki context.
      */
-    public DeleteAttachmentMetaDataTransactionRunnable(XWikiAttachment xwikiAttachment, XWikiContext xwikiContext)
+    public DeleteAttachmentMetaDataTransactionRunnable(XWikiAttachment xwikiAttachment, XWikiContext xwikiContext, boolean updateDoc)
     {
         this.xwikiAttachment = xwikiAttachment;
         this.xwikiContext = xwikiContext;
+        this.updateDoc = updateDoc;
     }
 
     @Override
@@ -82,7 +88,9 @@ public class DeleteAttachmentMetaDataTransactionRunnable extends TransactionRunn
             }
         }
 
-        xwikiContext.getWiki().getStore().saveXWikiDoc(xwikiAttachment.getDoc(), xwikiContext, false);
+        if (updateDoc) {
+            xwikiContext.getWiki().getStore().saveXWikiDoc(xwikiAttachment.getDoc(), xwikiContext, false);
+        }
 
         session.delete(xwikiAttachment);
     }
